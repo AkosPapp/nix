56500850b032a595259397598a81f89f605c16e1
Papp Akos <qq557702@gmail.com>
2024-03-08 22:07:25 +0100

    2024-03-08-22-07-25

--- modules/virtualization/virt-manager.nix
+++ modules/virtualization/virt-manager.nix
@@ -1,14 +1,14 @@
 {config, pkgs, lib, ... }:
 {
     options = {
-        MODULES.vitualisation.virt-manager.enable = lib.mkOption {
+        MODULES.virtualisation.virt-manager.enable = lib.mkOption {
             type = lib.types.bool;
             default = false;
             description = "Enable virt-manager";
         };
     };
 
-    config = lib.mkIf config.MODULES.vitualisation.virt-manager.enable {
+    config = lib.mkIf config.MODULES.virtualisation.virt-manager.enable {
         programs.virt-manager.enable = true;
         virtualisation.libvirtd.enable = true;
         programs.dconf.enable = true;
