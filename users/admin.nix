29499231127c50324dd696d49b36a828d8559276
Papp Akos <qq557702@gmail.com>
2024-03-09 21:05:18 +0100

    2024-03-09-21-05-18

--- users/admin.nix
+++ users/admin.nix
@@ -13,7 +13,7 @@
             isNormalUser = true;
             description = "Admin";
             extraGroups = [ "wheel" "docker" "input" "uinput" "plugdev" ];
-            hashedPassword = "$y$j9T$bOI0dJ3j75d4UcqG2rgf80$/.QPT9DzhdJPj41ixp2rO/EIm289kJFjISeSNU8xow2";
+            hashedPassword = "$y$j9T$3G1lwFZrgipfzE8NwHKjC1$mnTAupZ2cQ1WHuZNLK9TT3MkdgEB18LYn6LjFecpsu6";
             openssh.authorizedKeys.keys = [
                 "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCYoxGrmlgUcVfncMnlDONPk5RwSEqbiBpvT3c2m9MIDsYdYlLfnRN5YJPZgp8CHcPnPK2rboRiHAO00j545ci2CDeAp6zGKoOhX0q/3fES3ySqMG7lJ+cplxoXekSL9GNOQqSREDymqrMfqGy9OnupqcAZForX/k1aegi99TgZwbGMAK/UIzfdkQr49VVzYbaNR14rikIfjvi23s4bdP/KgeAs0T9KEKKSAd9WJQxUr/dmTjNBODzW10llgmCCVRNk3Pj4A1qiGiz0wkjG7XmZT0QjHyrX2GzSYuhW1l8s6mY3tTBBKVoj+peBphgxGBbEwUCQh0yPVBGstM5fHqN1bvOjRfYNQboVSmLhicX7Bk0WNLPS6DtqHZTXGNuYM8NcHn4xUIX5GwlsS6Mfo2tDMcX83w1Jv0BuImfcUMl6jvYCzcpEdGENYHWisIvQLSlAK6UEIYGeG8CH/iRqRPQIrOW49EQJYlW2VSLuTf8SA6c9Z2xdSIsli9JOfr79VUdYpgrdiv7vFjiX5d+hcJVC0rjQkF6XlAWVH5yMfpr1OXFbpKqILygx9Zcj7IhMHodQsjtr6+FjIs5Xm5Nt1nY9Cpke/q3lHcgq0PVgwvMPMhTOxfv6XoKQGmDTJWsmAP8n4BotZm7H2OlO29/zJnrgJ8+ZienkAX5s2cAaT16Kvw== akos@laptop"
             ];
