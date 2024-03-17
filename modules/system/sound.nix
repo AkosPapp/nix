fad565d55030eac724665cd387adcc14ee6c0d17
Papp Akos <qq557702@gmail.com>
2024-03-17 07:07:28 +0100

    2024-03-17-07-07-28

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -12,6 +12,9 @@
         security.rtkit.enable = true;
         services.pipewire = {
             enable = true;
+            audio.enable = true;
+            wireplumber.enable = true;
+            systemWide = true;
             alsa.enable = true;
             alsa.support32Bit = true;
             pulse.enable = true;
