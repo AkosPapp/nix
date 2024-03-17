50696865a75c3de2acb021f4257a5def5f76fcc0
Papp Akos <qq557702@gmail.com>
2024-03-17 15:59:12 +0100

    2024-03-17-15-59-12

--- flake.nix
+++ flake.nix
@@ -4,27 +4,27 @@
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-23.11";
         nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-#home-manager.url = "github:nix-community/home-manager";
-#home-manager.inputs.nixpkgs.follows = "nixpkgs";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
     outputs = { nixpkgs, nixpkgs-unstable, ... }:
-    let
+        let
         system = "x86_64-linux";
-        pkgs-unstable = import nixpkgs-unstable { inherit system; };
-        pkgs = import nixpkgs {
-            system = system;
-            config = {
-                allowUnfree = true;
-                permittedInsecurePackages = [
-                    "nix-2.16.2"
-                ];
-            };
+    pkgs-unstable = import nixpkgs-unstable { inherit system; };
+    pkgs = import nixpkgs {
+        system = system;
+        config = {
+            allowUnfree = true;
+            permittedInsecurePackages = [
+                "nix-2.16.2"
+            ];
         };
+    };
     in {
         nixosConfigurations = import ./nixos-configurations.nix {
             inherit nixpkgs system pkgs pkgs-unstable;
-            };
+        };
     };
 
 }
