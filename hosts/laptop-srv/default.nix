b22b194263cab292c1800b1e6bb760f14e3bdbb9
Papp Akos <qq557702@gmail.com>
2024-09-16 17:42:48 +0200

    2024-09-16-17-42-48

---
+++ hosts/laptop-srv/default.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [./hardware-configuration.nix];
+
+  #  # Bootloader.
+  #  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking = {
+    networkmanager.enable = true;
+    hostId = "68bf4e0e";
+    hostName = "laptop-srv";
+    extraHosts = ''
+      127.0.0.1 localhost
+    '';
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  system.stateVersion = "24.05";
+
+
+  MODULES.virtualisation.docker.enable = true;
+  USERS.admin.enable = true;
+
+  services.openssh.PermitRootLogin = lib.mkForce "prohibit-password";
+}
---
+++ hosts/laptop-srv/hardware-configuration.nix
@@ -0,0 +1,64 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+
+  config = {
+    boot = {
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+
+      initrd = {
+        availableKernelModules = ["nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
+        kernelModules = [];
+      };
+      kernelModules = ["kvm-amd"];
+      #extraModulePackages = [ ];
+      supportedFilesystems = ["zfs"];
+      zfs = {
+        extraPools = ["zroot"];
+        forceImportRoot = true;
+        allowHibernation = false;
+      };
+    };
+
+    fileSystems."/" = {
+      device = "zroot/root";
+      fsType = "zfs";
+    };
+
+    fileSystems."/nix" = {
+      device = "zroot/nix";
+      fsType = "zfs";
+    };
+
+    fileSystems."/etc/NetworkManager/system-connections" = {
+      device = "zroot/persist/system-connections";
+      fsType = "zfs";
+    };
+
+    fileSystems."/home" = {
+      device = "zroot/persist/home";
+      fsType = "zfs";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-partlabel/disk-samsung980-ESP";
+      fsType = "vfat";
+      options = ["fmask=0022" "dmask=0022"];
+    };
+
+    swapDevices = [
+      {device = "/dev/disk/by-partlabel/disk-samsung980-swap";}
+    ];
+
+    networking.useDHCP = lib.mkDefault true;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode =
+      lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+}
