6381265f0b901aefb811e2f9a4a95d6961008f4e
Papp Akos <qq557702@gmail.com>
2024-09-29 09:08:52 +0200

    2024-09-29-09-08-52

--- nixos-configurations.nix
+++ nixos-configurations.nix
@@ -1,67 +1,58 @@
-{
-  nixpkgs,
-  home-manager,
-  sops-nix,
-  disko,
-  ...
-} @ args: {
-  iso = nixpkgs.lib.nixosSystem {
-    specialArgs = args;
-    modules = [
-      ./generated.nix
-      ./hosts/iso
-      home-manager.nixosModules.home-manager
-      disko.nixosModules.disko
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      }
-      sops-nix.nixosModules.sops
-    ];
-  };
+{ nixpkgs, home-manager, sops-nix, disko, ... }@args: {
 
-  laptop = nixpkgs.lib.nixosSystem {
-    specialArgs = args;
-    modules = [
-      ./generated.nix
-      ./hosts/laptop
-      home-manager.nixosModules.home-manager
-      disko.nixosModules.disko
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      }
-      sops-nix.nixosModules.sops
-    ];
-  };
+    iso = nixpkgs.lib.nixosSystem {
+        specialArgs = args;
+        modules = [ ./generated.nix ./hosts/iso
+        home-manager.nixosModules.home-manager
+        disko.nixosModules.disko
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+        }
+        sops-nix.nixosModules.sops
+        ];
+    };
+    
 
-  laptop-srv = nixpkgs.lib.nixosSystem {
-    specialArgs = args;
-    modules = [
-      ./generated.nix
-      ./hosts/laptop-srv
-      home-manager.nixosModules.home-manager
-      disko.nixosModules.disko
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      }
-      sops-nix.nixosModules.sops
-    ];
-  };
+    laptop = nixpkgs.lib.nixosSystem {
+        specialArgs = args;
+        modules = [ ./generated.nix ./hosts/laptop
+        home-manager.nixosModules.home-manager
+        disko.nixosModules.disko
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+        }
+        sops-nix.nixosModules.sops
+        ];
+    };
+    
 
-  legion5 = nixpkgs.lib.nixosSystem {
-    specialArgs = args;
-    modules = [
-      ./generated.nix
-      ./hosts/legion5
-      home-manager.nixosModules.home-manager
-      disko.nixosModules.disko
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-      }
-      sops-nix.nixosModules.sops
-    ];
-  };
+    laptop-srv = nixpkgs.lib.nixosSystem {
+        specialArgs = args;
+        modules = [ ./generated.nix ./hosts/laptop-srv
+        home-manager.nixosModules.home-manager
+        disko.nixosModules.disko
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+        }
+        sops-nix.nixosModules.sops
+        ];
+    };
+    
+
+    legion5 = nixpkgs.lib.nixosSystem {
+        specialArgs = args;
+        modules = [ ./generated.nix ./hosts/legion5
+        home-manager.nixosModules.home-manager
+        disko.nixosModules.disko
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+        }
+        sops-nix.nixosModules.sops
+        ];
+    };
+    
 }
