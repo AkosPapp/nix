f1e70198f1fcdcee56e251960599a1fce211e20c
Papp Akos <qq557702@gmail.com>
2024-10-06 09:21:05 +0200

    2024-10-06-09-21-05

--- hosts/legion5/sops.nix
+++ hosts/legion5/sops.nix
@@ -1,16 +1,17 @@
 {sops-nix, ...}: {
-  imports = [<sops-nix/modules/sops>];
+  imports = [sops-nix.nixosModules.sops];
   # This will add secrets.yml to the nix store
   # You can avoid this by adding a string to the full path instead, i.e.
   # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
-  sops.defaultSopsFile = ./secrets/example.yaml;
+  sops.defaultSopsFile = ../../secrets/example.yaml;
+  sops.validateSopsFiles = false;
   # This will automatically import SSH keys as age keys
-  # sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   # This is using an age key that is expected to already be in the filesystem
-  sops.age.keyFile = "/home/akos/.config/sops/age/keys.txt";
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   # This will generate a new key if the key specified above does not exist
-  sops.age.generateKey = false;
+  sops.age.generateKey = true;
   # This is the actual specification of the secrets.
-  # sops.secrets.example-key = {};
+  sops.secrets.example-key = {};
   # sops.secrets."myservice/my_subdir/my_secret" = {};
 }
