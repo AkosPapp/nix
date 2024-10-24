907d528fb1b7408178ccc30105e2bcdcce870e32
Papp Akos <qq557702@gmail.com>
2024-10-24 18:05:06 +0200

    2024-10-24-18-05-06

--- hosts/legion5/default.nix
+++ hosts/legion5/default.nix
@@ -17,6 +17,8 @@
     '';
   };
 
+  networking.nameservers = ["100.100.100.100" "9.9.9.9" "8.8.8.8" "1.1.1.1" "172.16.0.1"];
+
   MODULES.system.printing.enable = true;
 
   users.users.root.hashedPassword = "$y$j9T$gEhP/0Jlrlwb4ndmLs06L1$7qkdPdgqjCrEH8bAQvJqRn/Mj4m5X9GCRAyM33z0mdA";
