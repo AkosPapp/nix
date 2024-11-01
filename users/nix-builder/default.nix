15899f9b9c550615a0e0aa62dc2fb47f9503d66d
Papp Akos <qq557702@gmail.com>
2024-11-01 12:48:17 +0100

    2024-11-01-12-48-17

--- users/nix-builder/default.nix
+++ users/nix-builder/default.nix
@@ -15,9 +15,11 @@
   config = lib.mkIf config.USERS.nix-builder.enable {
     users.groups.nix-builder = {};
     users.users.nix-builder = {
-      isSystemUser = true;
+      isNormalUser = true;
       description = "nix-builder";
       group = "nix-builder";
+      home = /var/empty;
+      createHome = false;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOavECvlGNeGwEWZASDDjU7XCZOQkO2XU2Zm1VKFWMME nix-builder"
       ];
