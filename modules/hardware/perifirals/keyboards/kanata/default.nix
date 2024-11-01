61c9b736089b376bb9d6aa6f227d0d68e7405143
Papp Akos <qq557702@gmail.com>
2024-11-01 19:36:03 +0100

    2024-11-01-19-36-03

--- modules/hardware/perifirals/keyboards/kanata/default.nix
+++ modules/hardware/perifirals/keyboards/kanata/default.nix
@@ -1,10 +1,25 @@
-{pkgs, ...}: {
-  systemd.services.kanata = {
-    enable = true;
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.kanata}/bin/kanata -c ${./config.kbd}";
-      User = "root";
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  options = {
+    MODULES.hardware.perifirals.keyboards.kanata.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable home row mods with kanata";
+    };
+  };
+
+  config = lib.mkIf config.MODULES.hardware.perifirals.keyboards.kanata.enable {
+    systemd.services.kanata = {
+      enable = true;
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        ExecStart = "${pkgs.kanata}/bin/kanata -c ${./config.kbd}";
+        User = "root";
+      };
     };
   };
 }
