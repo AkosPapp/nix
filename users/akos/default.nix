226472757e9bd893268e102ba99092e7f6fa711f
Papp Akos <qq557702@gmail.com>
2025-02-20 09:32:48 +0100

    2025-02-20-09-32-48

--- users/akos/default.nix
+++ users/akos/default.nix
@@ -132,7 +132,7 @@
           nbconvert
           jupyter
           seaborn
-          # sklearn-deap
+          sklearn-deap
           # mlxtend
           # (opencv4.override {
           #   enableGtk3 = true;
