569a0ef62750b764347a6f98260ee1cb538b92c4
Papp Akos <qq557702@gmail.com>
2025-03-26 20:10:33 +0100

    2025-03-26-20-10-33

--- modules/nix/builders/build-host.nix
+++ modules/nix/builders/build-host.nix
@@ -15,7 +15,7 @@
 
   config = lib.mkIf config.MODULES.nix.builders.build-host {
     MODULES.security.sops.enable = true;
-    sops.secrets."nix-builder/airlab/private_key" = {};
+    sops.secrets."nix-builder/build-host/private_key" = {};
     # remote build
     nix.buildMachines = [
       {
@@ -30,7 +30,7 @@
         speedFactor = 2;
         supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
         mandatoryFeatures = [];
-        sshKey = config.sops.secrets."nix-builder/airlab/private_key".path;
+        sshKey = config.sops.secrets."nix-builder/build-host/private_key".path;
       }
     ];
 
@@ -43,6 +43,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+c88YGotI3xhM2y8+RuUuVJD3sAt9jUtCaYEYJ1JHT akos@laptop"
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCYoxGrmlgUcVfncMnlDONPk5RwSEqbiBpvT3c2m9MIDsYdYlLfnRN5YJPZgp8CHcPnPK2rboRiHAO00j545ci2CDeAp6zGKoOhX0q/3fES3ySqMG7lJ+cplxoXekSL9GNOQqSREDymqrMfqGy9OnupqcAZForX/k1aegi99TgZwbGMAK/UIzfdkQr49VVzYbaNR14rikIfjvi23s4bdP/KgeAs0T9KEKKSAd9WJQxUr/dmTjNBODzW10llgmCCVRNk3Pj4A1qiGiz0wkjG7XmZT0QjHyrX2GzSYuhW1l8s6mY3tTBBKVoj+peBphgxGBbEwUCQh0yPVBGstM5fHqN1bvOjRfYNQboVSmLhicX7Bk0WNLPS6DtqHZTXGNuYM8NcHn4xUIX5GwlsS6Mfo2tDMcX83w1Jv0BuImfcUMl6jvYCzcpEdGENYHWisIvQLSlAK6UEIYGeG8CH/iRqRPQIrOW49EQJYlW2VSLuTf8SA6c9Z2xdSIsli9JOfr79VUdYpgrdiv7vFjiX5d+hcJVC0rjQkF6XlAWVH5yMfpr1OXFbpKqILygx9Zcj7IhMHodQsjtr6+FjIs5Xm5Nt1nY9Cpke/q3lHcgq0PVgwvMPMhTOxfv6XoKQGmDTJWsmAP8n4BotZm7H2OlO29/zJnrgJ8+ZienkAX5s2cAaT16Kvw== akos@laptop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOavECvlGNeGwEWZASDDjU7XCZOQkO2XU2Zm1VKFWMME nix-builder"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJPP+EWsn2LyMLqTuUUa6+o/toTgWWIZLsk4xG3shyyx nix-builder"
       ];
     };
 
