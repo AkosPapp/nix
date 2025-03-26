cb0d36b76bb415002a56921e5e889d4581a59a8f
Papp Akos <qq557702@gmail.com>
2025-03-26 21:00:57 +0100

    2025-03-26-21-00-57

--- modules/nix/builders/build-host.nix
+++ modules/nix/builders/build-host.nix
@@ -16,23 +16,22 @@
   config = lib.mkIf config.MODULES.nix.builders.build-host {
     MODULES.security.sops.enable = true;
     sops.secrets."nix-builder/private_key" = {};
-    # remote build
-    # nix.buildMachines = [
-    #   {
-    #     sshUser = "builder";
-    #     hostName = "localhost";
-    #     #system = "x86_64-linux";
-    #     protocol = "ssh-ng";
-    #     # if the builder supports building for multiple architectures,
-    #     # replace the previous line by, e.g.
-    #     systems = ["x86_64-linux" "aarch64-linux"];
-    #     maxJobs = 16;
-    #     speedFactor = 2;
-    #     supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-    #     mandatoryFeatures = [];
-    #     sshKey = config.sops.secrets."nix-builder/private_key".path;
-    #   }
-    # ];
+    nix.buildMachines = [
+      {
+        sshUser = "builder";
+        hostName = "localhost";
+        #system = "x86_64-linux";
+        protocol = "ssh-ng";
+        # if the builder supports building for multiple architectures,
+        # replace the previous line by, e.g.
+        systems = ["x86_64-linux" "aarch64-linux"];
+        maxJobs = 16;
+        speedFactor = 2;
+        supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+        mandatoryFeatures = [];
+        sshKey = config.sops.secrets."nix-builder/private_key".path;
+      }
+    ];
 
     users.users.builder = {
       createHome = true;
