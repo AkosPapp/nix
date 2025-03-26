957cfad71328bdc8ab6cee499cd8f28ce808a0ce
Papp Akos <qq557702@gmail.com>
2025-03-26 21:00:07 +0100

    2025-03-26-21-00-07

--- modules/nix/builders/build-host.nix
+++ modules/nix/builders/build-host.nix
@@ -17,22 +17,22 @@
     MODULES.security.sops.enable = true;
     sops.secrets."nix-builder/private_key" = {};
     # remote build
-    nix.buildMachines = [
-      {
-        sshUser = "builder";
-        hostName = "localhost";
-        #system = "x86_64-linux";
-        protocol = "ssh-ng";
-        # if the builder supports building for multiple architectures,
-        # replace the previous line by, e.g.
-        systems = ["x86_64-linux" "aarch64-linux"];
-        maxJobs = 16;
-        speedFactor = 2;
-        supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-        mandatoryFeatures = [];
-        sshKey = config.sops.secrets."nix-builder/private_key".path;
-      }
-    ];
+    # nix.buildMachines = [
+    #   {
+    #     sshUser = "builder";
+    #     hostName = "localhost";
+    #     #system = "x86_64-linux";
+    #     protocol = "ssh-ng";
+    #     # if the builder supports building for multiple architectures,
+    #     # replace the previous line by, e.g.
+    #     systems = ["x86_64-linux" "aarch64-linux"];
+    #     maxJobs = 16;
+    #     speedFactor = 2;
+    #     supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+    #     mandatoryFeatures = [];
+    #     sshKey = config.sops.secrets."nix-builder/private_key".path;
+    #   }
+    # ];
 
     users.users.builder = {
       createHome = true;
@@ -42,10 +42,8 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuOKEh8UO6InEyDHU+bZAR+WwPoy/NM5sX6RpEmDOXp root@legion5"
       ];
-      group = "nixbld";
+      group = "users";
     };
-    nix.settings.trusted-users = ["root" "builder"];
-    nix.settings.allowed-users = ["root" "builder"];
 
     nix.distributedBuilds = true;
     # optional, useful when the builder has a faster internet connection than yours
