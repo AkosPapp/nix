5bf096a98e5e872eb67aadd287d3367afc33ff92
Papp Akos <qq557702@gmail.com>
2025-09-05 09:37:31 +0200

    2025-09-05-09-37-31

--- modules/virtualization/virtualbox.nix
+++ modules/virtualization/virtualbox.nix
@@ -16,5 +16,7 @@
   config = lib.mkIf config.MODULES.virtualisation.virtualbox.enable {
     virtualisation.virtualbox.host.enable = true;
     virtualisation.virtualbox.host.enableExtensionPack = true;
+    virtualisation.virtualbox.host.enableKvm = true;
+    virtualisation.virtualbox.host.addNetworkInterface = false;
   };
 }
