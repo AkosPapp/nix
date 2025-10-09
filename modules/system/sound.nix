3354c10d00a1bf6f24bad9094e3955cc2b7bc72a
Papp Akos <qq557702@gmail.com>
2025-10-09 20:22:13 +0200

    2025-10-09-20-22-13

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -13,13 +13,13 @@
   };
   config = lib.mkIf config.MODULES.system.sound.enable {
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
+    #services.pipewire = {
+    #  enable = true;
+    #  alsa.enable = true;
+    #  alsa.support32Bit = true;
+    #  pulse.enable = true;
+    #  jack.enable = true;
+    #};
 
     environment.systemPackages = with pkgs; [
       pavucontrol
--- modules/wm/niri.nix
+++ modules/wm/niri.nix
@@ -28,10 +28,9 @@
     environment.sessionVariables.ELECTRON_OZONE_PLATFORM_HINT = "wayland";
     environment.sessionVariables.OZONE_PLATFORM = "wayland";
     services.upower.enable = true;
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
     environment.systemPackages = with pkgs; [
+      xdg-desktop-portal-gnome
       wireplumber
       niri
       swww
@@ -43,5 +42,37 @@
       fuzzel
       xwayland-satellite
     ];
+
+    # PipeWire (required for screencasting)
+    services.pipewire = {
+      enable = lib.mkForce true;
+      audio.enable = true; # If you want audio support
+      pulse.enable = true; # PulseAudio compatibility
+      jack.enable = false; # Set true only if you need JACK
+      alsa.enable = true;
+      alsa.support32Bit = true; # For 32-bit app support
+      wireplumber.enable = true;
+      socketActivation = true; # Recommended for modern setups
+      systemWide = false; # Should be false (per-user is safer)
+    };
+
+    # RTKit for real-time priority (recommended for PipeWire)
+    security.rtkit.enable = true;
+
+    # D-Bus (should already be enabled by default on NixOS)
+    services.dbus = {
+      enable = true; # Usually implicit
+      implementation = "broker"; # Default, can also be "dbus"
+    };
+
+    # XDG Desktop Portal for screencasting
+    xdg.portal = {
+      enable = true;
+      extraPortals = [pkgs.xdg-desktop-portal-gnome];
+      config.common.default = lib.mkForce "*"; # Or specify "gnome" explicitly
+    };
+
+    # Hardware audio support
+    hardware.alsa.enable = true; # Usually enabled by default
   };
 }
