9265ff2ca29bc629bf40588dc02bf1284a6cdd90
Papp Akos <qq557702@gmail.com>
2025-10-19 02:16:52 +0200

    2025-10-19-02-16-52

--- modules/system/pipewire.nix
+++ modules/system/pipewire.nix
@@ -17,25 +17,13 @@
     # PipeWire (required for screencasting)
     services.pipewire = {
       enable = true;
+      pulse.enable = true; # For PulseAudio app support
+      jack.enable = true; # For JACK app support
+      alsa.enable = true; # For ALSA app support
       alsa.support32Bit = true; # For 32-bit app support
       wireplumber.enable = true;
       socketActivation = true; # Recommended for modern setups
       systemWide = false; # Should be false (per-user is safer)
-      extraConfig.pipewire = {
-        "10-f64-processing" = {
-          "context.properties" = {
-            # Internal PipeWire clock and processing format
-            "default.clock.rate" = 48000;
-            "default.clock.allowed-rates" = [44100 48000 96000];
-            "default.clock.format" = "F64";
-
-            # Optional tuning for latency and buffer sizing
-            "default.clock.quantum" = 1024;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 8192;
-          };
-        };
-      };
     };
 
     environment.systemPackages = with pkgs; [
