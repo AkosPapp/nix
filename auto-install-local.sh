f9beec2b10475d082df5a8293d6c8a0931ba233e
Papp Akos <qq557702@gmail.com>
2025-12-01 10:48:22 +0100

    2025-12-01-10-48-22

--- auto-install-local.sh
+++ auto-install-local.sh
@@ -4,10 +4,7 @@ set -eou pipefail
 
 CONFIGURATION="akos01zfs"
 
-# disko 
-nix --experimental-features "nix-command flakes"run github:nix-community/disko/latest -- --mode destroy,format,mount --flake .#${CONFIGURATION} 
-
-
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --flake .#${CONFIGURATION} 
 
 nixos-install --flake .#${CONFIGURATION} --no-root-passwd
 
