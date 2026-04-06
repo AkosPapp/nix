020b3fda37bfa951481aa58bbdcab3ab15b345db
Papp Akos <qq557702@gmail.com>
2026-04-06 18:54:14 +0200

    2026-04-06-18-54-14

--- hosts/legion5/hardware-configuration.nix
+++ hosts/legion5/hardware-configuration.nix
@@ -72,45 +72,45 @@
       ])
       ++ [config.hardware.nvidia.package];
 
-    # boot.initrd.kernelModules = ["nvidia"];
-    # boot.extraModulePackages = [config.hardware.nvidia.package];
-    # hardware.nvidia = {
-    #  # Modesetting is required.
-    # modesetting.enable = true;
+    boot.initrd.kernelModules = ["nvidia"];
+    boot.extraModulePackages = [config.hardware.nvidia.package];
+    hardware.nvidia = {
+      # Modesetting is required.
+      modesetting.enable = true;
 
-    #  # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    #  # Enable this if you have graphical corruption issues or application crashes after waking
-    #  # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    #  # of just the bare essentials.
-    # powerManagement.enable = lib.mkForce true;
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = lib.mkForce true;
 
-    #  # Fine-grained power management. Turns off GPU when not in use.
-    #  # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    # powerManagement.finegrained = false;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
 
-    #  # Use the NVidia open source kernel module (not to be confused with the
-    #  # independent third-party "nouveau" open source driver).
-    #  # Support is limited to the Turing and later architectures. Full list of
-    #  # supported GPUs is at:
-    #  # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    #  # Only available from driver 515.43.04+
-    #  # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    #  open = false;
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
 
-    #  #nvidiaPersistenced = true;
+      #nvidiaPersistenced = true;
 
-    #  # Enable the Nvidia settings menu,
-    #  # accessible via `nvidia-settings`.
-    #  nvidiaSettings = lib.mkForce true;
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = lib.mkForce true;
 
-    #  # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    #    package = config.boot.kernelPackages.nvidiaPackages.latest;
-    #    prime = {
-    #      sync.enable = true;
-    #      nvidiaBusId = "PCI:1:0:0";
-    #      amdgpuBusId = "PCI:6:0:0";
-    #    };
-    # };
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+      # prime = {
+      #   sync.enable = true;
+      #   nvidiaBusId = "PCI:1:0:0";
+      #   amdgpuBusId = "PCI:6:0:0";
+      # };
+    };
     users.users.root.hashedPassword = "$y$j9T$gEhP/0Jlrlwb4ndmLs06L1$7qkdPdgqjCrEH8bAQvJqRn/Mj4m5X9GCRAyM33z0mdA";
   };
 }
